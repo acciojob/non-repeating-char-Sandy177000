829335b6f7221db8544f074828e01c24a30e9e4e
Sandesh <112424645+Sandy177000@users.noreply.github.com>
2024-01-15 12:37:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
+
 function firstNonRepeatedChar(str) {
- // Write your code here
+  // Write your code here
+  let map = new Map();
+
+  for (let index = 0; index < str.length; index++) {
+     
+   if(map.has(str[index])){
+     map.set(str[index],map.get(str[index])+1);
+   }
+   else{
+     map.set(str[index],1);
+   }
+ }
+
+ let ans = null;
+ for(const ch of str){
+   
+    if(map.get(ch)==1){
+      ans = ch;
+      break;
+    }
+ }
+
+ return ans;
+
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
